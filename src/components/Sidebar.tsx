dd4f1656670dfe9192806126950e6c1b085c9319
Jorge Letelier <jorge.letelierw@gmail.com>
2026-08-15 13:21:16 -0400

    Actualizar Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,12 @@
 import { NavLink } from 'react-router-dom';
-const items=['Dashboard','Mapa','Trabajos','Clima','Rios','Usuarios','Configuracion'];
+
+const items=[
+'Dashboard',
+'Carga',
+'Infraestructura',
+'Trabajos',
+'Clima',
+'Configuracion'
+];
+
 export default function Sidebar(){return <nav className="app-sidebar">{items.map(i=><NavLink key={i} to={`/${i.toLowerCase()}`} className={({isActive})=>`side-link ${isActive?'active':''}`}>{i}</NavLink>)}</nav>}
