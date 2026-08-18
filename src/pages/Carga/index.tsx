04b8a6a683ff5f9cdff57cd90c297100e2777103
Jorge Letelier <jorge.letelierw@gmail.com>
2026-08-17 22:16:51 -0400

    Refactor Carga component to use PagePanel

--- src/pages/Carga/index.tsx
+++ src/pages/Carga/index.tsx
@@ -1,6 +1,23 @@
 import type { AppData } from '../../types';
-import DataLoader from '../../components/DataLoader';
 
-export default function Carga({ data, setData }: { data: AppData; setData: (d: AppData) => void }) {
-  return <DataLoader data={data} setData={setData} />;
+import DataLoader from '../../components/DataLoader';
+import PagePanel from '../../components/PagePanel';
+
+interface CargaProps {
+  data: AppData;
+  setData: (data: AppData) => void;
+}
+
+export default function Carga({
+  data,
+  setData,
+}: CargaProps) {
+  return (
+    <PagePanel width="min(760px, calc(100% - 32px))">
+      <DataLoader
+        data={data}
+        setData={setData}
+      />
+    </PagePanel>
+  );
 }
