f32e3d2a7f5540d14b7a6cf8c12b78ab20506b89
Jorge Letelier <jorge.letelierw@gmail.com>
2026-08-18 01:03:32 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
-
-import './index.css';
 
 ReactDOM.createRoot(
   document.getElementById('root')!
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <div
+      style={{
+        background: 'white',
+        color: 'black',
+        padding: '50px',
+        fontSize: '32px',
+      }}
+    >
+      REACT FUNCIONA
+    </div>
   </React.StrictMode>
 );
