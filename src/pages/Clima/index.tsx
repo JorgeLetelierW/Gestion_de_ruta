4dfb73c17252e01618483c9fafb3907747a6ef13
Jorge Letelier <jorge.letelierw@gmail.com>
2026-08-17 22:40:30 -0400

    Refactor Clima component to use PagePanel

--- src/pages/Clima/index.tsx
+++ src/pages/Clima/index.tsx
@@ -1,1 +1,10 @@
-import WeatherPanel from '../../components/WeatherPanel'; export default function Clima(){return <WeatherPanel/>}
+import WeatherPanel from '../../components/WeatherPanel';
+import PagePanel from '../../components/PagePanel';
+
+export default function Clima() {
+  return (
+    <PagePanel width="min(440px, calc(100% - 32px))">
+      <WeatherPanel />
+    </PagePanel>
+  );
+}
